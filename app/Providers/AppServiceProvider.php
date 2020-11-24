8900e96bc63182936c3eb29b612fdb79a377c4a6
chaohuichen <chenpeter0821@gmail.com>
2020-11-23 19:00:43 -0500

    change back the lines

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if(env('APP_ENV') === 'production') {
+        if(env('APP_ENV','production') === 'production') {
                     \URL::forceScheme('https');
           }
     }
