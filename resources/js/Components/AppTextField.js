0307c7677f2dc28be3e2d84f69280ce4dfd0643e
chaohuichen <chenpeter0821@gmail.com>
2020-11-24 12:23:00 -0500

    added new desgin for login and production files:

--- resources/js/Components/AppTextField.js
+++ resources/js/Components/AppTextField.js
@@ -1,23 +1,7 @@
 import { TextField } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
 import { SetFormMessage } from './index'
 
-const useStyles = makeStyles(() => ({
-  root: {
-    padding: '20px 0'
-  },
-  input: {
-    color: 'black'
-  },
-  text: {
-    fontSize: 'medium',
-    fontStyle: 'italic',
-    fontWeight: 'bolder'
-  }
-}))
-
 const AppTextField = (props) => {
-  const classes = useStyles()
   return (
     <div
       style={{
@@ -27,16 +11,18 @@ const AppTextField = (props) => {
       }}
     >
       <TextField
-        // id='standard-basic'
-        className={classes.root}
-        InputLabelProps={{ className: classes.input, shrink: true }}
-        InputProps={{ className: classes.input }}
+        id={props.name}
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
         label={props.label}
         type={props.type}
         name={props.name}
         value={props.values[props.name]}
         onChange={props.handleChange}
         autoComplete={props.name}
+        autoFocus
       />
       {props.name in props.errors
         ? SetFormMessage(props.errors[props.name])
--- resources/js/pages/LogIn.js
+++ resources/js/pages/LogIn.js
@@ -3,8 +3,32 @@ import BaseForm from '../Components/BaseForm'
 import AppTextField from '../Components/AppTextField'
 import { auth } from '../store'
 import { connect } from 'react-redux'
+import { makeStyles, Avatar, CssBaseline, Typography, Container, Grid } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Link from '@material-ui/core/Link'
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    minHeight: '75vh'
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+}));
 
 const Login = ({ loginUser, error, history }) => {
+  const classes = useStyles();
   const ValidationSchema = Yup.object({
     email: Yup.string()
       .email('Invalid email address')
@@ -19,37 +43,50 @@ const Login = ({ loginUser, error, history }) => {
   const finalCommand = () => history.push('/')
 
   return (
-    <div className="form-page">
-      <div className="form-title">Log in to your account</div>
-      <span style={{ color: 'red' }}>{error}</span>
-      <BaseForm
-        initialValues={{ email: '', password: '' }}
-        validationSchema={ValidationSchema}
-        fastValidation
-        externalApi={{
-          insertDocument: loginUser
-        }}
-        finalCommand={finalCommand}
-        buttonText="Log In"
-      >
-        {(formProps) => (
-          <>
-            <AppTextField
-              {...formProps}
-              label="Email"
-              type="email"
-              name="email"
-            />
-            <AppTextField
-              {...formProps}
-              label="Password"
-              type="password"
-              name="password"
-            />
-          </>
-        )}
-      </BaseForm>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <span style={{ color: 'red' }}>{error}</span>
+        <BaseForm
+          initialValues={{ email: '', password: '' }}
+          validationSchema={ValidationSchema}
+          fastValidation
+          externalApi={{
+            insertDocument: loginUser
+          }}
+          finalCommand={finalCommand}
+          buttonText="Log In"
+        >
+          {(formProps) => (
+            <>
+              <AppTextField
+                {...formProps}
+                label="Email"
+                type="email"
+                name="email"
+              />
+              <AppTextField
+                {...formProps}
+                label="Password"
+                type="password"
+                name="password"
+              />
+              <Grid item space={5}>
+                <Link href="#" variant="body2">
+                  {'Don\'t have an account? Sign Up'}
+                </Link>
+              </Grid>
+            </>
+          )}
+        </BaseForm>
+      </div>
+    </Container>
   )
 }
 const mapState = (state) => ({ error: state.user.error })
