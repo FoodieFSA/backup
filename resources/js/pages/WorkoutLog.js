58cd5a0f64066550ca754c260d5f7a3ae89a57cf
chaohuichen <chenpeter0821@gmail.com>
2020-11-30 10:36:32 -0500

    allow user to edit the workoutlog name

--- resources/js/pages/WorkoutLog.js
+++ resources/js/pages/WorkoutLog.js
@@ -1,16 +1,18 @@
 import { useState } from 'react'
-import { Button } from '@material-ui/core'
+import { Button, IconButton, TextField } from '@material-ui/core'
 import produce from 'immer'
 import SingleExercise from '../Components/SingleExercise'
 import ExerciseModal from '../Components/ExerciseModal'
 import { connect } from 'react-redux'
-const WorkoutLog = ({ workoutLog }) => {
+import EditIcon from '@material-ui/icons/Edit';
+import { editWorkoutLogName } from '../store'
+const WorkoutLog = ({ workoutLog, editWorkoutLogName }) => {
   const [state, setState] = useState({
     workoutLogName: 'MONDAY WORKOUT',
     exerciseSets: [],
     modalOpen: false
   })
-
+  const [editName, setEditName] = useState(false)
   // Modal logic
   const toggleModal = () => {
     setState(produce((draftState) => {
@@ -20,7 +22,19 @@ const WorkoutLog = ({ workoutLog }) => {
 
   return (
     <div style={{ flex: 1 }}>
-      <h2 id="workoutlog-title">{workoutLog.workoutLogName}</h2>
+      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: '15px' }}>
+        <IconButton aria-label="delete" style={{ backgroundColor: 'gray', marginLeft: '15px', marginRight: '15px', width: '50px', height: '50px' }} onClick={() => setEditName(!editName)} >
+          <EditIcon style={{ color: 'white' }}/>
+        </IconButton>
+        {editName ? <TextField
+          style={{ marginLeft: '15px', marginRight: '15px' }}
+          value = {workoutLog.workoutLogName}
+          variant="outlined"
+          onChange={(e) => editWorkoutLogName(e.target.value)}
+        />
+          : <h2>{workoutLog.workoutLogName}</h2>
+        }
+      </div>
       {
         workoutLog && workoutLog.exercises.map((exercise, index) => {
           return (
@@ -41,5 +55,10 @@ const mapState = (state) => {
     workoutLog: state.workoutLog
   }
 }
+const mapDispatch = (dispatch) => {
+  return {
+    editWorkoutLogName: (userInput) => dispatch(editWorkoutLogName(userInput))
+  }
+}
 
-export default connect(mapState, null)(WorkoutLog)
+export default connect(mapState, mapDispatch)(WorkoutLog)
