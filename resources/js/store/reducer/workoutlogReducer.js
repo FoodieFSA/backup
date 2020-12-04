db41e03e116b17e6b8068582dfb537b958af6dcc
mkzheng972 <markzheng27@gmail.com>
2020-12-03 22:07:56 -0500

    worked on workoutlog: getworkoutlog, createworkoutlog, and updateworkout log with yan, deleted npm run prod as there is a continous loop

--- resources/js/store/reducer/workoutlogReducer.js
+++ resources/js/store/reducer/workoutlogReducer.js
@@ -1,38 +1,37 @@
-import Api from '../../Api'
-import produce from 'immer'
-import moment from 'moment'
+import Api from '../../Api';
+import produce from 'immer';
+import moment from 'moment';
 /**
  * ACTION TYPES
  */
 // for workouts
-const GOT_WORKOUTS = 'GOT_WORKOUTS'
-const CREATED_WORKOUT = 'CREATED_WORKOUT'
-const ADDED_EXERCISE = 'ADDED_EXERCISE'
-const REMOVED_EXERCISE = 'REMOVED_EXERCISE'
-const EDIT_WORKOUT_NAME = 'EDIT_WORKOUT_NAME'
+const GOT_WORKOUT = 'GOT_WORKOUT';
+const ADDED_EXERCISE = 'ADDED_EXERCISE';
+const REMOVED_EXERCISE = 'REMOVED_EXERCISE';
+const EDIT_WORKOUT_NAME = 'EDIT_WORKOUT_NAME';
 // for set
-const ADDED_SET = 'ADDED_SET'
-const UPDATED_SET_LBS = 'UPDATED_SET_LBS'
-const UPDATED_SET_REPS = 'UPDATED_SET_REPS'
-const REMOVED_SET = 'REMOVED_SET'
-const COMPLETE_SET = 'COMPLETE_SET'
+const ADDED_SET = 'ADDED_SET';
+const UPDATED_SET_LBS = 'UPDATED_SET_LBS';
+const UPDATED_SET_REPS = 'UPDATED_SET_REPS';
+const REMOVED_SET = 'REMOVED_SET';
+const COMPLETE_SET = 'COMPLETE_SET';
 // const SAVED = "SAVED"
 
 /**
  * INITIAL STATE
  */
 const initialStateWorkout = {
-
   // TODO: use moment to name workoutlogname today's date
-  workoutLogName: moment.now(), // day of month    DATE.NOW()
+  id: -1,
+  name: moment.now(), // day of month    DATE.NOW()
   userId: '',
   // adding exercise objects into the exercises array
   exercises: []
   // modalOpen: false,
-}
+};
 
 // initial state for set
-const emptySet = { lbs: 0, reps: 0, completeStatus: false }
+const emptySet = { lbs: 0, reps: 0, completeStatus: false };
 
 // {
 //   id:"",
@@ -41,19 +40,46 @@ const emptySet = { lbs: 0, reps: 0, completeStatus: false }
 // }
 
 // for exercise section...........................
-const gotWorkoutLog = (workout) => ({ type: GOT_WORKOUTS, workout })
-const createdWorkout = (workout) => ({ type: CREATED_WORKOUT, workout })
-export const addExercise = (exercise) => ({ type: ADDED_EXERCISE, exercise })
-export const removeExercise = (exerciseIndex) => ({ type: REMOVED_EXERCISE, exerciseIndex })
-export const editWorkoutLogName = (inputName) => ({ type: EDIT_WORKOUT_NAME, inputName })
+const gotWorkoutLog = (workout) => ({ type: GOT_WORKOUT, workout });
+
+export const addExercise = (exercise) => ({ type: ADDED_EXERCISE, exercise });
+export const removeExercise = (exerciseIndex) => ({
+  type: REMOVED_EXERCISE,
+  exerciseIndex
+});
+export const editWorkoutLogName = (inputName) => ({
+  type: EDIT_WORKOUT_NAME,
+  inputName
+});
 // for set section...........................
 // const removedSet = (exerciseId, set) => ({ type: REMOVED_SET, exerciseId, set })
 // const updatedSet = (exerciseId, set) => ({ type: UPDATED_SET, exerciseId, set })
-export const updateSetLbs = (exerciseIndex, setIndex, userInputLbs) => ({ type: UPDATED_SET_LBS, exerciseIndex, setIndex, userInputLbs })
-export const updateSetReps = (exerciseIndex, setIndex, userInputReps) => ({ type: UPDATED_SET_REPS, exerciseIndex, setIndex, userInputReps })
-export const toggleCompleteSet = (exerciseIndex, setIndex) => ({ type: COMPLETE_SET, exerciseIndex, setIndex })
-export const addExerciseSet = (exerciseIndex) => ({ type: ADDED_SET, exerciseIndex })
-export const removedSet = (exerciseIndex, setIndex) => ({ type: REMOVED_SET, exerciseIndex, setIndex })
+export const updateSetLbs = (exerciseIndex, setIndex, userInputLbs) => ({
+  type: UPDATED_SET_LBS,
+  exerciseIndex,
+  setIndex,
+  userInputLbs
+});
+export const updateSetReps = (exerciseIndex, setIndex, userInputReps) => ({
+  type: UPDATED_SET_REPS,
+  exerciseIndex,
+  setIndex,
+  userInputReps
+});
+export const toggleCompleteSet = (exerciseIndex, setIndex) => ({
+  type: COMPLETE_SET,
+  exerciseIndex,
+  setIndex
+});
+export const addExerciseSet = (exerciseIndex) => ({
+  type: ADDED_SET,
+  exerciseIndex
+});
+export const removedSet = (exerciseIndex, setIndex) => ({
+  type: REMOVED_SET,
+  exerciseIndex,
+  setIndex
+});
 /*
 TODO:
 - need to account for same day workout -> thinking to use Date as identifier for workout objects
@@ -64,173 +90,121 @@ TODO:
  */
 
 // thunk to get workout log, we are assuming user has field for an array of workoutlogs
-export const getWorkoutLog = (logId, userId) => async (dispatch) => {
+export const getWorkoutLog = (logId) => async (dispatch) => {
   try {
     // TODO: add workoutLogs field to user, and get the log id response
-    const response = await Api.get(`workoutlog/${logId}`)
+    const response = await Api.get(`workoutlog/${logId}`);
     if (response) {
-      const localWorkoutLog = JSON.parse(localStorage.getItem('workoutLog'))
-      if (localWorkoutLog.id === response.id) {
-        // Local Storage data has precedence over data base one. We just replace the one from the data base
-        gotWorkoutLog(localWorkoutLog)
-      } else {
-        gotWorkoutLog(response.data)
-      }
+      dispatch(gotWorkoutLog(response.data));
     } else {
-      const workoutLog = {
-        name: Date.now(), // Need more work to get the day only without time
-        userId: userId,
-        exercises: []
-      }
-      const response = await Api.post('/workoutlog/', workoutLog)
-      dispatch(createdWorkout(response.data))
-      // Create a local storage workout
-      // name, id, exercises
-      localStorage.setItem('workoutLog', JSON.stringify(response.data))
+      console.error('This record doesn\'t exist');
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
+
+export const createWorkoutLog = async (workout) => {
+  try {
+    // Need to change to have a name
+    // Need more work
+    await Api.post('workoutlog', workout);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // thunk to save changins of workoutlog to the backend
-// export const save = () => async (dispatch) => {
-//   try {
-//     // TODO: finish adding workoutlog id
+export const updateWorkoutLog = async (workout) => {
+  try {
+    // TODO: finish adding workoutlog id
 
-//     await Api.put('/workoutLog/id')
-//     dispatch(save())
-//   } catch (error) {
-//     console.error(error)
-//   }
-// }
+    await Api.put(`/workoutLog/${workout.id}`);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 /**
  * REDUCER
  */
-// export default function (state = initialStateWorkout, action) {
-//   switch (action.type) {
-//     case GOT_WORKOUTS:
-//       return action.workout
-//     case CREATED_WORKOUT:
-//       return action.workout
-//     case ADDED_EXERCISE:
-//       return { ...state, exercises: [...state.exercises, { ...action.exercise, sets: [{ ...emptySet }] }] }
-//     case REMOVED_EXERCISE:
-//       return { ...state, exercises: state.exercises.filter(exercise => exercise.id !== action.id) }
-//     case ADDED_SET: {
-//       const tempExercises = [...state.exercises]
-//       const oldExercises = tempExercises.map((exercise, index) => {
-//         if (index === action.exerciseIndex) {
-//           exercise.sets.push({ ...emptySet })
-//         }
-//         return { ...exercise }
-//       })
-//       return { ...state, exercises: oldExercises }
-//     }
-//     case UPDATED_SET:
-//       return {
-//         ...state,
-//         exercises: state.exercises.map(exercise => {
-//           if (exercise.id === action.exerciseId) {
-//             exercise.sets[action.set.id] = action.set
-//           }
-//           return exercise
-//         })
-//       }
-//     case REMOVED_SET: {
-//       const tempExercises = [...state.exercises]
-//       const oldExercises = tempExercises.map((exercise, index) => {
-//         if (index === action.exerciseIndex) {
-//           const tempSets = exercise.sets.filter((singleSet, index) => {
-//             if (index !== action.setIndex) { return { ...singleSet } }
-//           })
-//           return { ...exercise, sets: tempSets }
-//         }
-//         return { ...exercise }
-//       })
-//       return { ...state, exercises: oldExercises }
-//     }
-//     case COMPLETE_SET: {
-//       // const oldExercises = [...state.exercises]
-//       // const tempExercises = oldExercises.map((exercise, index) => {
-//       //   if (index === action.exerciseIndex) {
-//       //     return {
-//       //       ...exercise,
-//       //       sets: exercise.sets.map((singleSet, index) => {
-//       //         if (index === action.setIndex) {
-//       //           singleSet.completeStatus = !singleSet.completeStatus
-//       //         }
-//       //         return singleSet
-//       //       })
-//       //     }
-//       //   }
-//       //   return exercise
-//       // })
-//       // return { ...state, exercises: tempExercises }
-//         return produce(state,(draft)=>{
-//             draft.exercises[action.exerciseIndex].sets[action.setIndex].completeStatus=! draft.exercises[action.exerciseIndex].sets[action.setIndex].completeStatus
-//         })
-//     }
-//     default:
-//       return state
-//   }
-// }
 
 const workoutlogReducer = produce((draft, action) => {
   switch (action.type) {
+    case GOT_WORKOUT: {
+      return action.workout;
+    }
     // exercise action section..............................................................
     case EDIT_WORKOUT_NAME: {
-      draft.workoutLogName = action.inputName
-      return draft
+      draft.workoutLogName = action.inputName;
+      return draft;
     }
     case ADDED_EXERCISE: {
-      const tempExercise = { ...action.exercise, sets: [{ ...emptySet }] }
-      draft.exercises.push(tempExercise)
-      return draft
+      const tempExercise = {
+        ...action.exercise,
+        sets: [{ ...emptySet }]
+      };
+      draft.exercises.push(tempExercise);
+      return draft;
     }
     case REMOVED_EXERCISE: {
-      draft.exercises = draft.exercises.filter((singleExercise, index) => index !== action.exerciseIndex)
-      return draft
+      draft.exercises = draft.exercises.filter(
+        (singleExercise, index) => index !== action.exerciseIndex
+      );
+      return draft;
     }
     // set action section..............................................................
     case COMPLETE_SET: {
-      draft.exercises[action.exerciseIndex].sets[action.setIndex].completeStatus =
-                !draft.exercises[action.exerciseIndex].sets[action.setIndex].completeStatus
-      return draft
+      draft.exercises[action.exerciseIndex].sets[
+        action.setIndex
+      ].completeStatus = !draft.exercises[action.exerciseIndex].sets[
+        action.setIndex
+      ].completeStatus;
+      return draft;
     }
     case REMOVED_SET: {
-      draft.exercises[action.exerciseIndex].sets =
-                draft.exercises[action.exerciseIndex].sets.filter((singleSet, index) => index !== action.setIndex)
+      draft.exercises[action.exerciseIndex].sets = draft.exercises[
+        action.exerciseIndex
+      ].sets.filter((singleSet, index) => index !== action.setIndex);
 
       if (draft.exercises[action.exerciseIndex].sets.length === 0) {
-        draft.exercises = draft.exercises.filter((singleExercise, index) => index !== action.exerciseIndex)
+        draft.exercises = draft.exercises.filter(
+          (singleExercise, index) => index !== action.exerciseIndex
+        );
       }
-      return draft
+      return draft;
     }
     case ADDED_SET: {
-      draft.exercises[action.exerciseIndex].sets.push({ ...emptySet })
-      return draft
+      draft.exercises[action.exerciseIndex].sets.push({ ...emptySet });
+      return draft;
     }
     case UPDATED_SET_LBS: {
       if (action.userInputLbs >= 0) {
-        draft.exercises[action.exerciseIndex].sets[action.setIndex].lbs = action.userInputLbs
+        draft.exercises[action.exerciseIndex].sets[
+          action.setIndex
+        ].lbs = action.userInputLbs;
       } else {
-        alert('The pound can not accept negative input number,please try the number that bigger than zero!')
+        alert(
+          'The pound can not accept negative input number,please try the number that bigger than zero!'
+        );
       }
-      return draft
+      return draft;
     }
     case UPDATED_SET_REPS: {
       if (action.userInputReps >= 0) {
-        draft.exercises[action.exerciseIndex].sets[action.setIndex].reps = action.userInputReps
+        draft.exercises[action.exerciseIndex].sets[
+          action.setIndex
+        ].reps = action.userInputReps;
       } else {
-        alert('The repetition can not accept negative input number,please try the number that bigger than zero!')
+        alert(
+          'The repetition can not accept negative input number,please try the number that bigger than zero!'
+        );
       }
-      return draft
+      return draft;
     }
     default:
-      return draft
+      return draft;
   }
-}, initialStateWorkout)
+}, initialStateWorkout);
 
-export default workoutlogReducer
+export default workoutlogReducer;
